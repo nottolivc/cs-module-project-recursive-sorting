690a56c21a57d56e8544a9fc3eb1e8e52f7bf44f
Andrew Nottoli <andrewnottoli@users-iMac.local>
2020-07-14 18:30:14 -0700

    recursive_binary

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,21 @@
 # TO-DO: Implement a recursive implementation of binary search
-def binary_search(arr, target, start, end):
-    # Your code here
+def binary_search_recursive(data, target, start, high):
+	if low > high:
+		return False
+	else:
+		mid = (low + high) // 2
+		if target == data[mid]:
+			return True
+		elif target < data[mid]:
+			return binary_search_recursive(data, target, low, mid-1)
+		else:
+			return binary_search_recursive(data, target, mid+1, high)
+
+
+data = [2,4,5,7,8,9,12,14,17,19,22,25,27,28,33,378]
+target = 378
+
+print(binary_search_recursive(data, target, 0, len(data)-1))
 
 
 # STRETCH: implement an order-agnostic binary search
